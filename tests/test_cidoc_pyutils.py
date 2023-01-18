c3eacef5756cc09a957a81c4d2244e16536ca795
csae8092 <peter.andorfer@oeaw.ac.at>
2023-01-18 09:05:43 +0100

    init commit

---
+++ tests/test_cidoc_pyutils.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestTestTest(unittest.TestCase):
+    """Tests for `acdh_cidoc_pyutils` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_001_gnd_to_geonames(self):
+        self.assertEqual(1, 1)
