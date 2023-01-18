5dbc596fd7ddc190e0eb36e121806cfd5fcf13f9
csae8092 <peter.andorfer@oeaw.ac.at>
2023-01-18 11:24:04 +0100

    wip: added create_e52 function

--- acdh_cidoc_pyutils/__init__.py
+++ acdh_cidoc_pyutils/__init__.py
@@ -1,5 +1,7 @@
 import uuid
-from rdflib import Literal, URIRef, XSD
+from rdflib import Graph, Literal, URIRef, XSD, RDF, RDFS
+
+from acdh_cidoc_pyutils.namespaces import CIDOC
 
 
 def date_to_literal(date_str: str) -> Literal:
@@ -17,9 +19,29 @@ def date_to_literal(date_str: str) -> Literal:
 
 
 def make_uri(domain="https://foo.bar/whatever", version="", prefix="") -> URIRef:
-    if domain.endswith('/'):
+    if domain.endswith("/"):
         domain = domain[:-1]
     some_id = f"{uuid.uuid1()}"
     uri_parts = [domain, version, prefix, some_id]
     uri = "/".join([x for x in uri_parts if x != ""])
     return URIRef(uri)
+
+
+def create_e52(uri: URIRef, begin_of_begin="", end_of_end="", label=True) -> Graph:
+    g = Graph()
+    g.add((uri, RDF.type, CIDOC["E52_Time-Span"]))
+    if begin_of_begin != "":
+        g.add((uri, CIDOC["P82a_begin_of_the_begin"], date_to_literal(begin_of_begin)))
+    if end_of_end != "":
+        g.add((uri, CIDOC["P82b_end_of_the_end"], date_to_literal(end_of_end)))
+    if end_of_end == "" and begin_of_begin != "":
+        g.add((uri, CIDOC["P82b_end_of_the_end"], date_to_literal(begin_of_begin)))
+    if begin_of_begin == "" and end_of_end != "":
+        g.add((uri, CIDOC["P82a_begin_of_the_begin"], date_to_literal(end_of_end)))
+    else:
+        pass
+    if label:
+        label_str = " - ".join([begin_of_begin, end_of_end]).strip()
+        if label_str != "":
+            g.add((uri, RDFS.label, Literal(label_str, datatype=XSD.string)))
+    return g
