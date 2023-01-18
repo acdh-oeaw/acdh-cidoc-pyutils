511127660450d4cf9ad7672fb14e1bf256dfae94
csae8092 <peter.andorfer@oeaw.ac.at>
2023-01-18 10:03:44 +0100

    feat: `date_to_literal()` added

--- README.md
+++ README.md
@@ -5,6 +5,30 @@
 # acdh-cidoc-pyutils
 Helper functions for the generation of CIDOC CRMish RDF
 
+## Usage
+
+* install via `pip install acdh-cidoc-pyutils`
+
+### date-like-string to casted rdflib.Literal
+
+```python
+from acdh_cidoc_pyutils import date_to_literal
+dates = [
+    "1900",
+    "1900-01",
+    "1901-01-01",
+    "foo",
+]
+for x in dates:
+    date_literal = date_to_literal(x)
+    print((date_literal.datatype))
+
+# returns
+# http://www.w3.org/2001/XMLSchema#gYear
+# http://www.w3.org/2001/XMLSchema#gYearMonth
+# http://www.w3.org/2001/XMLSchema#date
+# http://www.w3.org/2001/XMLSchema#string
+```
 
 ## development
 
--- tests/test_cidoc_pyutils.py
+++ tests/test_cidoc_pyutils.py
@@ -1,5 +1,16 @@
 import unittest
 
+from acdh_cidoc_pyutils import date_to_literal
+
+DATE_STRINGS = ["1900", "-1900", "1900-01", "1901-01-01", "foo"]
+DATE_TYPES = [
+    "http://www.w3.org/2001/XMLSchema#gYear",
+    "http://www.w3.org/2001/XMLSchema#gYear",
+    "http://www.w3.org/2001/XMLSchema#gYearMonth",
+    "http://www.w3.org/2001/XMLSchema#date",
+    "http://www.w3.org/2001/XMLSchema#string",
+]
+
 
 class TestTestTest(unittest.TestCase):
     """Tests for `acdh_cidoc_pyutils` package."""
@@ -12,3 +23,8 @@ class TestTestTest(unittest.TestCase):
 
     def test_001_gnd_to_geonames(self):
         self.assertEqual(1, 1)
+
+    def test_002_dates(self):
+        for i, x in enumerate(DATE_STRINGS):
+            date_literal = date_to_literal(x)
+            self.assertEqual(f"{date_literal.datatype}", DATE_TYPES[i])
