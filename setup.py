41df62f4e0a6182f1842c1a0cfb2231a50b54198
csae8092 <peter.andorfer@oeaw.ac.at>
2023-02-22 12:39:24 +0100

    version bump -> v0.15

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     test_suite="tests",
     tests_require=[],
     url="https://github.com/acdh-oeaw/acdh-cidoc-pyutils",
-    version="v0.13",
+    version="v0.15",
     zip_safe=False,
 )
